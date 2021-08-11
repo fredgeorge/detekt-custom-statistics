35bb87764effd78c0eb17ee99ad8f932011b41f8
GitHub Action <action@github.com>
2021-08-11 06:34:20 +0000

    Change package name from org.example.detekt to com.github.fredgeorge.detektcustomstatistics

--- src/main/kotlin/com/github/fredgeorge/detektcustomstatistics/MyRule.kt
+++ src/main/kotlin/com/github/fredgeorge/detektcustomstatistics/MyRule.kt
@@ -1,4 +1,4 @@
-package org.example.detekt
+package com.github.fredgeorge.detektcustomstatistics
 
 import io.gitlab.arturbosch.detekt.api.CodeSmell
 import io.gitlab.arturbosch.detekt.api.Config
--- src/test/kotlin/com/github/fredgeorge/detektcustomstatistics/MyRuleTest.kt
+++ src/test/kotlin/com/github/fredgeorge/detektcustomstatistics/MyRuleTest.kt
@@ -1,4 +1,4 @@
-package org.example.detekt
+package com.github.fredgeorge.detektcustomstatistics
 
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.test.compileAndLint
